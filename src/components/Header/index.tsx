e2fa0eecbfd92c55688999dfa35ac0dcfd553396
SteveHerison <steveherisondev@gmail.com>
2024-09-20 09:09:11 -0300

    Finalizando o site, precisando de alguns ajustes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import Logo from "../../assets/Logo.png";
-import Lupa from "../../assets/Lupa.svg";
+// import Lupa from "../../assets/Lupa.svg";
 import Cart from "../../assets/cart.svg";
 import { CarrinhoContext } from "../../contexts/UserContext";
 
@@ -9,7 +9,13 @@ const Header = () => {
 
   if (!carrinhoContext) return null;
 
-  const { handleOpenModal } = carrinhoContext;
+  const { handleOpenModal, produtosSelecionados } = carrinhoContext;
+
+  // Somar a quantidade total de produtos no carrinho
+  const totalProdutos = produtosSelecionados.reduce(
+    (acc, produto) => acc + produto.quantidade,
+    0
+  );
 
   return (
     <div className="w-full h-20 bg-red-50">
@@ -27,15 +33,15 @@ const Header = () => {
             placeholder="Buscar"
             className="border pl-8 pr-5 rounded-xl focus:outline-none focus:border-orange-950"
           /> */}
-          <img src={Lupa} alt="Lupa" className="w-5 h-5 absolute left-2" />
+          {/* <img src={Lupa} alt="Lupa" className="w-5 h-5 absolute left-2" /> */}
         </div>
-        <button>
-          <img
-            src={Cart}
-            alt="Carrinho"
-            className="w-6 h-6 mx-3"
-            onClick={handleOpenModal}
-          />
+        <button className="relative" onClick={handleOpenModal}>
+          <img src={Cart} alt="Carrinho" className="w-6 h-6 mx-3" />
+          {totalProdutos > 0 && (
+            <span className="absolute -top-3 left-7 bg-red-600 text-white text-xs font-bold w-5 h-5 rounded-full flex items-center justify-center">
+              {totalProdutos}
+            </span>
+          )}
         </button>
       </div>
     </div>
